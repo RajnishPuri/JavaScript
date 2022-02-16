b29f361dcc8af13770a49a117075c18d60b6a101
RajnishPuri <rajnishpuri15@gmail.com>
2022-02-16 14:50:48 +0530

    type_conversion

---
+++ 16-02-22/type_conversion.js
@@ -0,0 +1,25 @@
+console.log(10 + " object");
+console.log("5" * "6");
+console.log(7);
+// let n=1 - "x";
+// console.log(n + " objects");
+let check="one";
+check=Number(check);
+
+console.log(check);
+
+let check2=['a'];
+check2=toString(check2);
+// check2=join(check2);
+console.log(check2);
+
+let n=17;
+let mydecimal= n.toString();
+let binary= "0b" + n.toString(2);
+let octal= "0o" + n.toString(8);
+let hex= "0x" + n.toString(16);
+
+console.log(mydecimal);
+console.log(binary);
+console.log(octal);
+console.log(hex);

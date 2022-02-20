f339b8482d2ec828ea4539e25ae0e2cff0119594
RajnishPuri <rajnishpuri15@gmail.com>
2022-02-21 01:17:31 +0530

    switch

---
+++ 21-02-22/switch.js
@@ -0,0 +1,12 @@
+let x=prompt("Enter the number");
+// prompt(x);
+x=Number(x);
+// console.log(x);
+switch(x){
+    case 1:
+        console.log("hello");
+        break;
+    case 2:
+        console.log("hello 2");
+        break;
+}

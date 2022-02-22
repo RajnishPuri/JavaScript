18557a7abc5425019e79059e1e9ca0fe02f3097c
RajnishPuri <rajnishpuri15@gmail.com>
2022-02-22 11:35:21 +0530

    array

---
+++ 22-02-22/array.js
@@ -0,0 +1,25 @@
+let arr = ["raj", 1, 's', "34", null, undefined];
+console.log(typeof arr);
+console.log(Array.isArray(arr));
+console.log(arr);
+// console.log(Array.isAsrray(obj));
+let obj ={};
+console.log(typeof obj);
+console.log(Array.isArray(obj));
+console.log(typeof arr[2]);
+
+let str="rajnish";
+console.log(typeof str);
+
+arr.push("new");
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift("hello");
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+

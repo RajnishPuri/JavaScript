7926d9c3bfe9df6db8f8effbe85d76a8a230047d
RajnishPuri <rajnishpuri15@gmail.com>
2022-02-22 11:36:13 +0530

    loop

---
+++ 22-02-22/for_loop.js
@@ -0,0 +1,11 @@
+let arr = ["hello", "raj"];
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+let arr2 = [];
+for (let i = 0; i< arr.length; i++){
+    arr2.push(arr[i].toUpperCase());
+}
+console.log(arr2); 

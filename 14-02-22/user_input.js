e743e26da02361368a2954a4a726ff1be7514728
RajnishPuri <rajnishpuri15@gmail.com>
2022-02-23 09:47:44 +0530

    multiplication_table

--- 14-02-22/user_input.js
+++ 14-02-22/user_input.js
@@ -1,7 +1,22 @@
-let number = prompt("Enter the Number");
-if(number<18){
-    console.log("You Can Not Drive");
-}
-else{
-    console.log("You Can drive");
-}
+// let number = prompt("Enter the Number");
+// if(number<18){
+//     console.log("You Can Not Drive");
+// }
+// else{
+//     console.log("You Can drive");
+// }
+// let name="Raj"
+// let filename="Bill";
+// let linenumber=25;
+// let greeting=`Hello ${name}.`;
+// console.log(greeting);
+
+// // ` is backticks
+// let filename="hello";
+// let errormessage=`\
+// \u2718 test failure at ${filename}:${linenumber}:
+// ${exception.message}
+// Stack trace:
+// ${exception.stack}`;
+
+console.log("\u2718");

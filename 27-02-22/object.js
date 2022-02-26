3b4d035368e79411676550ee34349bbd915ad77b
RajnishPuri <rajnishpuri15@gmail.com>
2022-02-26 15:33:47 +0530

    object

---
+++ 27-02-22/object.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id     :  5566
+  };
+  
+  // Display some data from the object:
+  document.getElementById("demo").innerHTML =
+  person["firstName"] + " " + person["lastName"];

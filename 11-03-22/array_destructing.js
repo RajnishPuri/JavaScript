007757544433057d774ef284a39955a5c36513d2
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-11 11:22:47 +0530

    array destructing

---
+++ 11-03-22/array_destructing.js
@@ -0,0 +1,12 @@
+const array=["value1", "value2"];
+let [array1, array2]=array;
+
+console.log("First array is",array1);
+console.log("Second array is",array2);
+
+const newarray=["value1"];
+let [newarray1, newarray2]=newarray;
+
+console.log("First array is",newarray1);
+console.log("Second array is",newarray2);
+

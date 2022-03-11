98e41752111068382ec442aaa37e5e9e12f919c9
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-11 11:28:18 +0530

    array destructing

--- 11-03-22/array_destructing.js
+++ 11-03-22/array_destructing.js
@@ -10,3 +10,10 @@ let [newarray1, newarray2]=newarray;
 console.log("First array is",newarray1);
 console.log("Second array is",newarray2);
 
+//for skipingsome value we have to left its position empty and a comma
+//like here value 2 is skipped and againnewarray2 stored the value value3
+const againnewarray=["value1", "value2", "value3"];
+let [againnewarray1, , againnewarray2]=againnewarray;
+
+console.log("First array is",againnewarray1);
+console.log("Second array is",againnewarray2);

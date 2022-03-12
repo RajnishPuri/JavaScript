2c6b57248c0b8dd7d6fff4e538fe8e2eb2cdaaf1
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-12 11:56:50 +0530

    var vs let vs const

---
+++ 12-03-22/var.js
@@ -0,0 +1,59 @@
+// x = 5; // Assign 5 to x
+// y=10;
+// // elem = document.getElementById("demo"); // Find an element
+// // elem.innerHTML = x;                     // Display x in the element
+
+// var x; // Declare x
+// console.log(x);
+// console.log(y);
+// let p=x+y;
+// // console.log(p);
+// var x; // Declare x
+// x = 5; // Assign 5 to x
+
+// elem = document.getElementById("demo"); // Find an element
+// elem.innerHTML = x;   
+
+
+// x=9;
+// let x;
+// console.log(x);
+
+// x=9;
+// const x;
+// console.log(x);
+
+// var x = 5; // Initialize x
+// var y = 7; // Initialize y
+
+// elem = document.getElementById("demo"); // Find an element
+// elem.innerHTML = x + " " + y;           // Display x and y
+
+// var x = 5; // Initialize x
+
+// // elem = document.getElementById("demo"); // Find an element
+// // elem.innerHTML = x + " " + y;           // Display x and y
+
+// // console.log(x+y);   IT RETURNS NAN
+
+// var y = 7; // Initialize y
+
+// console.log(x+y);
+// console.log(elem.innerHTML);
+
+// var x = 5; // Initialize x
+// var y;     // Declare y
+
+// // elem = document.getElementById("demo"); // Find an element
+// // elem.innerHTML = x + " " + y;           // Display x and y
+
+// console.log(x+y);
+
+// y = 7;    // Assign 7 to y
+
+
+let [first, ...rest]="hello";
+console.log(first, rest);
+
+const{sin, cos, tan}=Math;
+console.log(siner, cos, tan);

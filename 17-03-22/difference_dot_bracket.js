627d1775799353234464fd9a5f5ead3c76441a0d
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-17 11:44:51 +0530

    Difference Between Dot and Bracket

---
+++ 17-03-22/difference_dot_bracket.js
@@ -0,0 +1,32 @@
+// difference between dot and bracket notation
+
+const person = {
+    name : "Raj",
+    Age : 23,
+    "Person Hobbies" : ["chess", "cricket"],   // if there is spaces between names or punctuation characters, or when it is a number(for arrays), you must use the SQUARE BRACKET NOTATION
+};
+
+console.log(person["Person Hobbies"]);
+console.log(person["name"]);
+console.log(person["Age"]);
+
+// if we want to use value of key1 instead of key1
+// here in person we get : 
+//  { name: "Raj", Age: 23, "Person Hobbies": (2) […], key1: "rajnishpuri15@gmail.com" }
+// And we don't want to write key1 we try to use it's value email instead of key1
+// this happen because we wrote person["key1"]="rajnishpuri15@gmail.com"; where key1 is in double quotes so person take key1 as string and use it as value
+
+const key1="email";
+person["key1"]="rajnishpuri15@gmail.com";
+console.log(person);
+
+// if you want to use value of key
+// you just have to remove the double quotation marks from key
+
+const key="email";
+person[key]="rajnishpuri15@gmail.com";
+console.log(person);
+
+for(let key5 in person){
+    console.log(`${[key5]} : ${person[key5]}`);
+}

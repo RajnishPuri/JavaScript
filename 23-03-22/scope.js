84837f13f05a9026d950468f8f2973ad911d29a0
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-23 13:14:44 +0530

    scope

---
+++ 23-03-22/scope.js
@@ -0,0 +1,34 @@
+// block scope vs function scope
+
+// let and const work on block scope
+// var work on function scope
+
+// {
+//     let game = "raj";    
+//     console.log(game);
+// }
+// console.log(game);
+
+// {
+//     let name ="puri";
+//     console.log(game);
+// }
+
+// {
+//     var game = "raj";
+//     console.log(game);
+// }
+// console.log(game);
+// {
+//     var game = "rajnish";
+//     console.log(game);
+// }
+
+// let game = "raj";
+// let game ="puri";
+// console.log(game);
+
+
+var Name ="raj";
+var Name ="puri";
+console.log(Name);

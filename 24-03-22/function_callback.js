b3a531f6aa10f357b52ac9093607cd33cfe9f4b7
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-24 11:45:17 +0530

    callback

--- 24-03-22/function_callback.js
+++ 24-03-22/function_callback.js
@@ -1,15 +1,34 @@
 // fuction callback
 
-function Myfunc2(){
+function Myfunc2(name){
     console.log("Inside my fuction 2");
+    console.log(`My name is ${name}`);
 }
 
 function Myfunc1(callback){
-    callback();
+    console.log("I'm Inside Myfunc1");
+    callback("Raj");
 }
 
 Myfunc1(Myfunc2);
 
 
+function newfunc(){
+     console.log("hello world!");
+}
 
-// 
+function display(a){
+    console.log(a);
+}
+
+display(newfunc);
+// Higher Order Function
+function func1(){
+    function hello(){
+    console.log("hello world");
+}
+return hello;
+}
+
+const ans=func1();
+console.log(ans);

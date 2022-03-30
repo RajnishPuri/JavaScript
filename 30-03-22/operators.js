fac476ec2904baf50c7aa68b0f04719b2ff14197
RajnishPuri <rajnishpuri15@gmail.com>
2022-03-30 23:35:55 +0530

    Operators

---
+++ 30-03-22/operators.js
@@ -0,0 +1,54 @@
+// + operators
+// usestrict
+console.log(1+2);
+console.log("1"+"2");
+console.log(1+"2");
+console.log(1+{});
+console.log(true + true + true - true);
+// console.log(false + false + false - true);
+console.log(1+null);
+console.log(1+undefined);
+let result;
+result=(-1)
+console.log(result);
+let x ="1";
+console.log(x+1);
+console.log(++x);
+console.log(x);
+console.log(true===true);
+console.log(true===false);
+console.log(null===undefined);
+console.log(null===null);
+console.log(null===NaN);
+console.log(NaN===NaN);
+console.log(-0===0);
+console.log("hello"=="ebent");
+console.log("hello"=="hello");
+console.log("hello"=="hello");
+console.log("hello"=="ehllo");
+console.log(false==0);
+console.log(false===0);
+console.log("34"<40);
+console.log("34"<"4");
+
+let data = [7,8,9,10];
+console.log("0" in data);
+console.log("1" in data);
+console.log("2" in data);
+console.log("3" in data);
+console.log("4" in data);
+console.log(1 in data);
+console.log(2 in data);
+console.log(3 in data);
+console.log(4 in data);
+console.log(5 in data);
+console.log(6 in data);
+console.log(0 in data);
+console.log(7 in data);
+console.log(8 in data);
+console.log(9 in data);
+console.log(10 in data);
+let num=7;
+console.log(num.toString());
+console.log(num);
+

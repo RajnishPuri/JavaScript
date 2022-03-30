da8f3379c0c35ea0d0cfeb257f8e6aa461764ee9
Spartan_Raj <raj@localhost.localdomain>
2022-03-30 11:33:24 +0530

    fill method

---
+++ 29-03-22/fill_method.js
@@ -0,0 +1,13 @@
+// Fill method
+// value, initial, final
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+array.fill(11, 3, 5);
+
+console.log(array);
+
+const myarray = new Array(10).fill(5);
+
+console.log(myarray);
+
+

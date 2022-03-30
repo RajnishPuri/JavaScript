d5e7535ab6c0148c89b8849cf0fef63ed49b8bda
Spartan_Raj <raj@localhost.localdomain>
2022-03-30 11:43:30 +0530

    splice method

---
+++ 29-03-22/splice_method.js
@@ -0,0 +1,20 @@
+// Splice Method
+// Start, Delete, Insert
+
+const myArray = [1, 2, 3, 4, 5];
+
+// deleting the item
+
+myArray.splice(1, 2);
+console.log(myArray);
+
+// Inserting the item and deleting the item
+
+myArray.splice(1, 1, 2, 3, 5);
+
+// first 1 means starting point i.e. 4
+// second 1 means deleting 1 value start from 4 i.e. 4
+// remaining value is for insertion after deletion takes place insert the value from index 1
+// answer will be : 1, 2, 3, 4, 5
+
+console.log(myArray);
